fbe36cb270b2567c5a24bb8f2203a6e5bc595ee6
Antonio Saenz Jr <tonisnz@Antonios-MacBook-Pro.local>
2025-09-17 10:45:03 -0500

    adding eventType

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,16 @@ const POSTS_QUERY = `*[
   && defined(slug.current)
 ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
 
+const EVENTS_Query = `*[
+  _type == "event"
+  && defined(slug.current)
+  ]{_id, name, slug, eventType}`;
+
 const options = { next: { revalidate: 30 } };
 
 export default async function IndexPage() {
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  const events = await client.fetch<SanityDocument[]>(EVENTS_Query, {}, options);
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
@@ -22,6 +28,22 @@ export default async function IndexPage() {
             <Link href={`/${post.slug.current}`}>
               <h2 className="text-xl font-semibold">{post.title}</h2>
               <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <h1>Events</h1>
+      <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
+        {events.map((event) => (
+          <li className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-sm dark:shadow-gray-900/20" key={event._id}>
+            <Link className="hover:underline block" href={`/events/$event?.slug?.current`}>
+              <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+                {event?.name}
+              </h2>
+              <p className="text-m font-semibold text-gray-900 dark:text-white">
+                {event?.eventType}
+              </p>
             </Link>
           </li>
         ))}
